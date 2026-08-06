640e22ab4cfc472387c1266b6213db927396d8d9
Toby Arculli <tobyarculli@Tobys-MacBook-Air-4.local>
2026-08-06 13:45:44 -0700

    added launcher

--- helium_inlet_app/app.py
+++ helium_inlet_app/app.py
@@ -292,3 +292,7 @@ async def websocket_endpoint(websocket: WebSocket):
             await asyncio.sleep(1.0)
     except (WebSocketDisconnect, Exception):
         pass
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
