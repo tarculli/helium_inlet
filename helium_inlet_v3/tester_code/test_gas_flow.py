edf4397237977bddd59e035b1d91cc576c405464
Toby Arculli <tobyarculli@Tobys-MacBook-Air-4.local>
2026-09-14 12:02:39 -0700

    adding some testing code for the gas flow

---
+++ helium_inlet_v3/tester_code/test_gas_flow.py
@@ -0,0 +1,13 @@
+import math
+import time
+import serial 
+
+from config import (
+    AGILENT_BAUD,
+    AGILENT_PORT,
+    STATE_VALVE_MAP,
+    TC_CHANNELS,
+    VALVE_CHANNELS,
+    VALVE_SLOT_PREFIX,
+    VOLT_CHANNELS,
+)
